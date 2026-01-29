b2a221c28e68cd3a176bb771a853fa0e9a7941ea
ngarnsworthy <ngarnsworthy@gmail.com>
2026-01-29 14:47:13 -0600

    Run spotless

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -2,31 +2,32 @@ package frc.robot;
 
 /** The Constants class holds all "Magic Numbers" and other constants */
 public final class Constants {
-  private Constants() {
-  }
+  private Constants() {}
 
   /** This class holds constants related to controlling the robot */
   public static class Control {
-    private Control() {
-    }
+    private Control() {}
 
     /** This is the gamepad port for the driver's controller */
     public static final int DRIVER_CONTROLLER_PORT = 0;
+
     /** This is the gamepad port for the co-driver's controller */
     public static final int COOP_CONTROLLER_PORT = 1;
   }
 
   /** This class holds constants related to the drive system */
   public static class Drivetrain {
-    private Drivetrain() {
-    }
+    private Drivetrain() {}
 
     /** The motor id for the motor in the front left of the bot */
     public static final int FRONT_LEFT_MOTOR = 1;
+
     /** The motor id for the motor in the front right of the bot */
     public static final int FRONT_RIGHT_MOTOR = 2;
+
     /** The motor id for the motor in the back left of the bot */
     public static final int BACK_LEFT_MOTOR = 3;
+
     /** The motor id for the motor in the back right of the bot */
     public static final int BACK_RIGHT_MOTOR = 4;
 
@@ -39,8 +40,7 @@ public final class Constants {
 
   /** This class holds constants used in tests */
   public static class Tests {
-    private Tests() {
-    }
+    private Tests() {}
 
     /** Maximum allowed deviation for tests using doubles */
     public static final double DELTA = 1e-2;
