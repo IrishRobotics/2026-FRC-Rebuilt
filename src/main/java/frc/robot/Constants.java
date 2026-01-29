cb8856d80030b51d9c72e3a0204cf37008ab5b49
ngarnsworthy <ngarnsworthy@gmail.com>
2026-01-29 13:15:36 -0600

    Fix formatting

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,12 +1,17 @@
 package frc.robot;
 
 public final class Constants {
+  private Constants() {}
   public static class Control {
+    private Control() {}
+
     public static final int driverControllerPort = 0;
     public static final int coopControllerPort = 1;
   }
 
   public static class Drivetrain {
+    private Drivetrain() {}
+
     public static final int frontLeft = 1;
     public static final int frontRight = 2;
     public static final int backLeft = 3;
@@ -17,6 +22,7 @@ public final class Constants {
   }
 
   public static class Tests {
+    private Tests() {}
     public static final double DELTA = 1e-2;
   }
 }
